2edefaed986875e61ffe167abc9e2afe07c16ff0
Thabo Lebelo <49479538+thabo-lebelo@users.noreply.github.com>
2019-04-10 12:25:37 +0200

    This is my first API

---
+++ my_api.py
@@ -0,0 +1,46 @@
+#THIS IS A WEBSERVER FOR DEMONSTRATING THE TYPES OF RESPONSES WE SEE FROM AN API ENDPOINT
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+#This is the data to manipulate
+developers = [{"id": "1", "name": "Thabo", "team": "Front End"}, {"id": "2", "name": "Vincent", "team": "Front End"}, {"id": "3", "name": "Rachel", "team": "Backend"}]
+
+#GET REQUEST
+@app.route('/get', methods = ['GET'])
+def getRequestHello():
+    #return "Hi, I got your GET Request!"
+    return jsonify({'result': 'Hello, this is a GET request!'}) 
+
+#GET REQUEST
+@app.route('/godb', methods = ['GET'])
+def getAllDevelopers():
+    developers_names = {}
+    for developer in developers:
+        developers_names.update(developer)
+        print("Name : " + developers_names["name"])
+    return jsonify({"Developers": developers})
+
+#POST REQUEST
+@app.route('/post', methods = ['POST'])
+def postRequestHello():
+    #return "I see you sent a POST message :-)"
+    return jsonify({'result': 'I see you sent a POST message :-)'}) 
+
+#UPDATE REQUEST
+@app.route('/update', methods = ['PUT'])
+def updateRequestHello():
+    #return "Sending Hello on an PUT request!"
+    return jsonify({'result': 'Sending Hello on an PUT request!'})
+
+#DELETE REQUEST
+@app.route('/delete', methods = ['DELETE'])
+def deleteRequestHello():
+    #return "Deleting your hard drive.....haha just kidding! I received a     DELETE request!"
+    return jsonify({'result': "Deleting your instances.....haha just kidding! I received a DELETE request!"})
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=5000)  
